93ad2d0699bee52da88388a6b134b77d83b5c94f
YONSAI <YONSAI@402-00>
2021-07-07 12:18:50 +0900

    "test1"

---
+++ javaTest/src/kr/co/job/BasicTest01.java
@@ -0,0 +1,39 @@
+/* 설명문 작성할수 있다.
+ * 단축키 
+ * 1. Ctrl + Space
+ * 2. Alt + 방향키(up/down)  : 커서기준으로 문장이 이동함.
+ * 3. Ctrl + Alt + 방향키(up/down) :  커서의 문장이 복사됨.
+ * 4. Ctrl + f : 찾기
+ */
+package kr.co.job;
+
+public class BasicTest01 {
+
+	public static void main(String[] args) {
+		//두수(3,4)와 결과값 선언  [자료형 변수명]
+		int num1 = 3;  //첫 수 int num1 = 0;  
+		int num2 = 4;  // 두번째 수
+		int result; // 결과값
+		//int num1, num2, result;
+		
+		//키보드 입력 : 잠시 패스
+		
+		// 처리
+		result = num1 + num2;
+		
+		// 출력
+		System.out.println("결과값(result) : " + result);	
+		
+		
+		
+		
+		
+		
+		
+		System.out.println("Welcome to java!!!");
+		System.out.println("Welcome to java!!!");
+		System.out.println("Welcome to java!!!");
+
+	}
+
+}
